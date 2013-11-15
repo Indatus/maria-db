9b59d8e7f2a085a597853236209a014dbb8af8a6
Brian Webb <bwebb@indatus.com>
2013-11-15 13:09:17 -0500

    updates to handle cluster joining & password setup

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['mariadb']['join_cluster_node']    = ""
+default['mariadb']['mysqladmin_bin']       = "/usr/bin/mysqladmin"
+default['mariadb']['mysql_bin']            = "/usr/bin/mysql"
+default['mariadb']['server_root_password'] = ""
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,11 @@
 #include apt to get updated
 include_recipe 'apt'
 
+
 package "python-software-properties" do
     action :install
 end
+
 
 apt_repository 'mariadb-server' do
     uri          'http://ftp.osuosl.org/pub/mariadb/repo/5.5/ubuntu'
@@ -14,14 +16,17 @@ apt_repository 'mariadb-server' do
     deb_src      true
 end
 
+
 apt_preference 'mariadb.pref' do
     glob         '*'
     pin          'origin http://ftp.osuosl.org/pub/mariadb/repo/5.5/ubuntu'
     pin_priority '1000'
 end
 
+
 #include build-essential for compiling C software
 include_recipe 'build-essential'
+
 
 package_list = {
     'libmysqlclient18' => '5.5.33a+maria-1~precise',
@@ -30,9 +35,39 @@ package_list = {
     'galera' => nil   
 }
 
+
 package_list.each do |pkg, ver|
     package pkg do
         version ver
         action :install
     end
+end
+
+
+execute "assign-root-password" do
+  command "\"#{node['mariadb']['mysqladmin_bin']}\" -u root password \"#{node['mariadb']['server_root_password']}\""
+  action :run
+  only_if "\"#{node['mariadb']['mysql_bin']}\" -u root -e 'show databases;'"
+end
+
+
+service "mysql" do
+  supports(
+    :start => true,
+    :stop => true, 
+    :restart => true,
+    :reload => true,
+    :status => true
+  )
+  action :enable
+end
+
+
+template "/etc/mysql/my.cnf" do
+  source "my.cnf.erb"
+  mode "644"
+  owner "root"
+  group "root"
+  notifies :restart, "service[mysql]"
+  variables :wsrep_cluster_address => node['mariadb']['join_cluster_node']
 end
